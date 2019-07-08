2f44edb57edf4a8c9626dc97255bb8063526611e
Daranii <Daranii@users.noreply.github.com>
2019-07-08 21:24:48 +0300

    Added pause menu and renamed scene.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused;
+
+    public GameObject pauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void Menu()
+    {
+        // TO DO: Saving
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        // TO DO: Saving
+        Application.Quit();
+    }
+}
